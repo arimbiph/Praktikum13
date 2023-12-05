012f59922ba65b41fda5dcf9b13b0ba11b942c2d
arimbiph <putriarimbi714@gmail.com>
2023-12-05 21:28:09 +0700

    Praktikum13

--- LocalGlobal06.java
+++ LocalGlobal06.java
@@ -4,7 +4,7 @@ public class LocalGlobal06 {
     public static void main(String[] args) {
       int b = 10;
       int c = 17;
-      a = a + 8;
+     a = a + 8;
       
       System.out.println("Nilai a: " + a);
       System.out.println("Nilai b: " + b);
@@ -17,6 +17,7 @@ public class LocalGlobal06 {
         int b = 20;
         int c = 5;
         a = a + 12;
+       
 
         System.out.println("Nilai a: " + a);
         System.out.println("Nilai b: " + b);
